9836590fa6141b11b00677422f67db136f70ac0a
Shulha Nikita <shulgamalthael@gmail.com>
2021-08-16 11:36:02 +0300

    fix build storage

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
     entry: "./src/index.jsx",
     output: {
       filename: "bundle.js",
-      path: path.resolve(__dirname, 'public'),
+      path: path.resolve(__dirname, 'build'),
       publicPath: "/", 
     },
     module: {
